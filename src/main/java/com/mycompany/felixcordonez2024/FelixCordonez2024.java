b02fa591aafbc4d6553605a918cbc962685e822c
Felix Cordonez <felix.08benaicazar@gamail.com>
2024-12-05 10:16:38 -0500

    PRUEBACICLO1

---
+++ src/main/java/com/mycompany/felixcordonez2024/FelixCordonez2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.felixcordonez2024;
+
+/**
+ *
+ * @author HP
+ */
+public class FelixCordonez2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
